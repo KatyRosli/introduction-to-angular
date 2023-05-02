a4ea2fa564d2fdba1395b039b56d3e8a6b7085e3
Katy Rosli <katyrosli@gmail.com>
2023-05-02 09:42:54 +0200

    search functions and results

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { HousingLocation } from '../housing-location';
 
 @Component({
@@ -7,22 +7,27 @@ import { HousingLocation } from '../housing-location';
   styleUrls: ['./housing-list.component.css']
 })
 export class HousingListComponent implements OnInit {
-    @Input() locationList: HousingLocation[] = [];
-    results: HousingLocation[] = [];
-    searchHousingLocations(searchText: string) {
-      if (!searchText) return;
-      this.results = this.locationList.filter(
-        (location: HousingLocation) => location.city
-        .toLowerCase()
-        .includes(
-          searchText.toLowerCase()
-        )
-      )
-  }
+  @Input() locationList: HousingLocation[] = [];
+  @Output() locationSelectedEvent = new EventEmitter<HousingLocation>();
+  results: HousingLocation[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  searchHousingLocations(searchText: string) {
+    if( !searchText) return;
+    this.results = this.locationList.filter(
+      (location: HousingLocation) => location.city
+      .toLowerCase()
+      .includes(
+        searchText.toLowerCase()
+      )
+    );
+  }
+  selectHousingLocation(location: HousingLocation) {
+    this.locationSelectedEvent.emit(location);
   }
 
 }
